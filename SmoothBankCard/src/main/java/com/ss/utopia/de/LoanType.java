2e18d84337b548eca374b514358720eb63c17af6
Parker-Williams-Smoothstack <parker.williams@smoothstack.com>
2021-05-07 14:06:30 -0400

    Loan DE, Loan DAO, LoanType

---
+++ SmoothBankCard/src/main/java/com/ss/utopia/de/LoanType.java
@@ -0,0 +1,73 @@
+package com.ss.utopia.de;
+
+/**
+ * Mainly used since JDBC does not handle enums. Maintains the status or type of a loan such as if it is paid off, on hold, etc. 
+ * @author Parker W.
+ *
+ */
+public class LoanType {
+	
+	private Integer id;
+	private String description;
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LoanType other = (LoanType) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "LoanType [" + (id != null ? "id=" + id + ", " : "")
+				+ (description != null ? "description=" + description : "") + "]";
+	}
+	
+	
+
+}
