9ea0d11b7dc72c99d02ac8b99c265e2040fbf740
Parker-Williams-Smoothstack <parker.williams@smoothstack.com>
2021-05-07 12:30:27 -0400

    Initial commit

---
+++ SmoothBankCard/src/main/java/com/ss/utopia/SmoothBankCardApplication.java
@@ -0,0 +1,13 @@
+package com.ss.utopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothBankCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoothBankCardApplication.class, args);
+	}
+
+}
