f3b02e819de55141b7d23bbe1485f6b7a6689f50
Parker-Williams-Smoothstack <parker.williams@smoothstack.com>
2021-05-12 10:58:04 -0400

    Clean redo of client code

--- SmoothBankCard/client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import cardTypeReducer from './cardTypeReducer';
-
-const rootReducer = combineReducers({
-  cardTypeReducer,
-});
-
-export default rootReducer;
